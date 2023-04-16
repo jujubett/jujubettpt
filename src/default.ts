b0fb1874b2eddd54753acdcefeb058036dcd5815
jujubett <119918408+jujubett@users.noreply.github.com>
2023-04-16 12:10:17 +0800

    Add files via upload

---
+++ src/default.ts
@@ -0,0 +1,11 @@
+export const defaultSetting = {
+  continuousDialogue: false,
+  archiveSession: false,
+  openaiAPIKey: "",
+  openaiAPITemperature: 60,
+  systemRule: ""
+}
+
+export const defaultMessage = `
+- 由 [OpenAI API (gpt-3.5-turbo)](https://platform.openai.com/docs/guides/chat) 提供支持，本网站无需科学上网，任意环境均可使用。
+- <kbd>Shift</kbd> + <kbd>Enter</kbd> 换行。开头输入 <kbd>/</kbd> 或者 <kbd>空格</kbd> 搜索 Prompt 预设。点击输入框滚动到底部。`
